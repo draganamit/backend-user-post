d8b4a789c85a78461e063398b7828d2a18a99edd
Slaven <slaven.cvi@hotmail.com>
2022-03-26 20:59:31 +0100

    Implementation of IPostService interface

--- Services/PostService/PostService.cs
+++ Services/PostService/PostService.cs
@@ -2,11 +2,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dotnet_users_posts.Models;
 
 namespace dotnet_users_posts.Services.PostService
 {
     public class PostService : IPostService
     {
-        
+        public List<Posts> AddPost(Posts newPost)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Posts> GetAllPosts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Posts GetPostById(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
